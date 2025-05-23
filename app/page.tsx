cc265e7a3af9d8b27ee4ed04d4cc94905298bbd8
sikulja <boris.sikuljak@gmail.com>
2025-05-23 11:16:31 +0200

    Implement ESLint & Prettier

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,10 @@
 const Home = () => {
   return (
-    <div><h1 className="text-3xl text-violet-700 font-black">Welcome to Next.js 👋</h1></div>
-  )
-}
-export default Home
+    <div>
+      <h1 className="text-3xl text-violet-700 font-black">
+        Welcome to Next.js 👋
+      </h1>
+    </div>
+  );
+};
+export default Home;
