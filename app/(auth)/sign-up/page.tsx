bed59770ce004a3a91d1eaa62a8bd8649ef9c36c
sikulja <boris.sikuljak@gmail.com>
2025-06-04 14:34:37 +0200

    MobileNav and NavLinks

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,4 +1,15 @@
+"use client";
+import AuthForm from "@/components/forms/AuthForm";
+import { SignUpSchema } from "@/lib/validation";
+
 const SignUp = () => {
-  return <div>SignUp</div>;
+  return (
+    <AuthForm
+      formType="SIGN_UN"
+      schema={SignUpSchema}
+      defaultValues={{ email: "", password: "", name: "", username: "" }}
+      onSubmit={(data) => Promise.resolve({ success: true, data })}
+    />
+  );
 };
 export default SignUp;
